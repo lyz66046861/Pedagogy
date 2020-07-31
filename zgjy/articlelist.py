43327fae4bb92b456bcfcc011c799a32ef2d2f0c
lyz66046861 <761977365@qq.com>
2020-08-01 02:06:46 +0800

    Update 教育学.xmind
    废弃

---
+++ zgjy/articlelist.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+from urllib import request
+import xlwt
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc.zh/#descendants
+max_page = 1
+for value in range(max_page):
+    value0 = str(value+2)
+    url = "http://www.zgjsks.com/html/jszp/ggjc/edu/" + value0 + ".html"
+    '''此行可以自行更换代码用来汇集数据'''
+    response = request.urlopen(url)
+    html = response.read()
+    # html = html.decode("html.parser")
+    bs = BeautifulSoup(html, 'lxml')
+    print(bs.find_all(class_="c_list"))
+    for link in bs.find_all(class_="c_list"):
+        print(link.get_text(),link.a['href'])
